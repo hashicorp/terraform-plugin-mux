94394e1912e0ac4d26c1bf1cb2087e36892b9169
Brian Flad <bflad417@gmail.com>
2022-02-16 10:48:52 -0500

    all: Add package-level Go documentation
    Reference: https://github.com/hashicorp/terraform-plugin-mux/issues/48

---
+++ tf6to5server/doc.go
@@ -0,0 +1,12 @@
+// Translate a protocol version 6 provider server into protocol version 5.
+//
+// Supported protocol version 6 provider servers include any which implement
+// the github.com/hashicorp/terraform-plugin-go/tfprotov6.ProviderServer
+// interface, such as:
+//
+// - github.com/hashicorp/terraform-plugin-framework
+// - github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server
+// - github.com/hashicorp/terraform-plugin-mux/tf5to6server
+//
+// Refer to the DowngradeServer() function for wrapping a server.
+package tf6to5server
