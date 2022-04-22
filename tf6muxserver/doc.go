786cfb5d1bbec094f426ebbaeeaa8e598671cf99
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-04-22 19:06:56 +0100

    Fixing the formatting of the generated godoc for the 4 key packages of this library (#75)

--- tf6muxserver/doc.go
+++ tf6muxserver/doc.go
@@ -1,12 +1,12 @@
-// Combine multiple protocol version 6 provider servers into a single server.
+// Package tf6muxserver combines multiple provider servers that implement protocol version 6, into a single server.
 //
 // Supported protocol version 6 provider servers include any which implement
-// the github.com/hashicorp/terraform-plugin-go/tfprotov6.ProviderServer
+// the tfprotov6.ProviderServer (https://pkg.go.dev/github.com/hashicorp/terraform-plugin-go/tfprotov6#ProviderServer)
 // interface, such as:
 //
-// - github.com/hashicorp/terraform-plugin-framework
-// - github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server
-// - github.com/hashicorp/terraform-plugin-mux/tf5to6server
+//     - https://pkg.go.dev/github.com/hashicorp/terraform-plugin-framework
+//     - https://pkg.go.dev/github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server
+//     - https://pkg.go.dev/github.com/hashicorp/terraform-plugin-mux/tf5to6server
 //
 // Refer to the NewMuxServer() function for creating a combined server.
 package tf6muxserver
--- tf6to5server/doc.go
+++ tf6to5server/doc.go
@@ -1,12 +1,12 @@
-// Translate a protocol version 6 provider server into protocol version 5.
+// Package tf6to5server translates a provider that implements protocol version 6, into one that implements protocol version 5.
 //
 // Supported protocol version 6 provider servers include any which implement
-// the github.com/hashicorp/terraform-plugin-go/tfprotov6.ProviderServer
+// the tfprotov6.ProviderServer (https://pkg.go.dev/github.com/hashicorp/terraform-plugin-go/tfprotov6#ProviderServer)
 // interface, such as:
 //
-// - github.com/hashicorp/terraform-plugin-framework
-// - github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server
-// - github.com/hashicorp/terraform-plugin-mux/tf6muxserver
+//     - https://pkg.go.dev/github.com/hashicorp/terraform-plugin-framework
+//     - https://pkg.go.dev/github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server
+//     - https://pkg.go.dev/github.com/hashicorp/terraform-plugin-mux/tf6muxserver
 //
 // Refer to the DowngradeServer() function for wrapping a server.
 package tf6to5server
