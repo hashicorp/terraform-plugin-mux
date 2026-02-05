e5beb2fb798c5dddf583d4ebdfd8b53e2bc4083b
Rain <rainskwan@gmail.com>
2026-02-05 12:24:24 -0500

    Updated license years

--- internal/logging/doc.go
+++ internal/logging/doc.go
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2020, 2025
+// Copyright IBM Corp. 2020, 2026
 // SPDX-License-Identifier: MPL-2.0
 
 // Package logging contains shared environment variable and log functionality.
