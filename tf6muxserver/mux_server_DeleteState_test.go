b2c1aff2d4b64b1bf677eceeee947ad87ce95def
Austin Valle <austinvalle@gmail.com>
2026-02-06 16:54:26 -0500

    added tests

---
+++ tf6muxserver/mux_server_DeleteState_test.go
@@ -0,0 +1,77 @@
+// Copyright IBM Corp. 2020, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package tf6muxserver_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+
+	"github.com/hashicorp/terraform-plugin-mux/internal/tf6testserver"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
+)
+
+func TestMuxServerStateStore_DeleteState(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	testServer1 := &tf6testserver.TestServer{
+		GetMetadataResponse: &tfprotov6.GetMetadataResponse{
+			StateStores: []tfprotov6.StateStoreMetadata{
+				{
+					TypeName: "test_statestore_server1",
+				},
+			},
+		},
+	}
+	testServer2 := &tf6testserver.TestServer{
+		GetMetadataResponse: &tfprotov6.GetMetadataResponse{
+			StateStores: []tfprotov6.StateStoreMetadata{
+				{
+					TypeName: "test_statestore_server2",
+				},
+			},
+		},
+	}
+
+	servers := []func() tfprotov6.ProviderServer{testServer1.ProviderServer, testServer2.ProviderServer}
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, servers...)
+
+	if err != nil {
+		t.Fatalf("unexpected error setting up mux server: %s", err)
+	}
+
+	_, err = muxServer.ProviderServer().(tfprotov6.StateStoreServer).DeleteState(ctx, &tfprotov6.DeleteStateRequest{
+		TypeName: "test_statestore_server1",
+	})
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if !testServer1.DeleteStateCalled["test_statestore_server1"] {
+		t.Errorf("expected test_statestore_server1 DeleteState to be called on server1")
+	}
+
+	if testServer2.DeleteStateCalled["test_statestore_server1"] {
+		t.Errorf("unexpected test_statestore_server1 DeleteState called on server2")
+	}
+
+	_, err = muxServer.ProviderServer().(tfprotov6.StateStoreServer).DeleteState(ctx, &tfprotov6.DeleteStateRequest{
+		TypeName: "test_statestore_server2",
+	})
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if !testServer2.DeleteStateCalled["test_statestore_server2"] {
+		t.Errorf("expected test_statestore_server2 DeleteState to be called on server2")
+	}
+
+	if testServer1.DeleteStateCalled["test_statestore_server2"] {
+		t.Errorf("unexpected test_statestore_server2 DeleteState called on server1")
+	}
+}
